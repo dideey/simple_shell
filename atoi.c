363c9aa9d3829f8edf1447129e39f8be84367ea3
dideey <bradleyjd32@gmail.com>
2023-07-25 06:04:25 -0700

    betty files

--- atoi.c
+++ atoi.c
@@ -2,81 +2,82 @@
 #include <unistd.h>
 
 /**
-* interactive - return 0 if non_interactive mode is on
-* confirms the initial mode thats on
-* @info: structure address
-*Return: true or false, 1 or 0.
-*/
+ * interactive - return 0 if non_interactive mode is on
+ * confirms the initial mode thats on
+ * @info: structure address
+ *Return: true or false, 1 or 0.
+ */
 
 int interactive(info_t *info)
 {
-    if (isatty(STDIN_FILENO) && info != NULL && info->readfd <= 2)
-{
-        return (1); /*true*/
-}
-     else
-{
-        return (0); /*false*/
-}
+	if (isatty(STDIN_FILENO) && info != NULL && info->readfd <= 2)
+	{
+		return (1); /*true*/
+	}
+	else
+	{
+		return (0); /*false*/
+	}
 
 }
 
 /**
-*_atoi - converts a string to an integer
-*@str: the string to be converted
-*Return: 0 if no numbers in string, converted number otherwise
-*/
+ *_atoi - converts a string to an integer
+ *@str: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
+ */
 
 int _atoi(char *str)
 {
-    int signMultiplier = 1; /* sign*/
-    unsigned int convertedValue = 0; /* the outcome result*/
-    int digitFlag = 0; /*the resignated  flag*/
+	int signMultiplier = 1; /* sign*/
+	unsigned int convertedValue = 0; /* the outcome result*/
+	int digitFlag = 0; /*the resignated  flag*/
 
-    while (*str != '\0' && digitFlag != 2)
-    {
-        if (*str == '-')
-            signMultiplier *= -1;
-        else if (*str >= '0' && *str <= '9')
-        {
-            digitFlag = 1;
-            convertedValue = convertedValue * 10 + (*str - '0');
-        }
-        else if (digitFlag == 1)
-            digitFlag = 2;
+	while (*str != '\0' && digitFlag != 2)
+	{
+		if (*str == '-')
+			signMultiplier *= -1;
+		else if (*str >= '0' && *str <= '9')
+		{
+			digitFlag = 1;
+			convertedValue = convertedValue * 10 + (*str - '0');
+		}
+		else if (digitFlag == 1)
+			digitFlag = 2;
 
-        str++;
-    }
+		str++;
+	}
 
-    return (signMultiplier == -1) ? -convertedValue : convertedValue;
+	return ((signMultiplier == -1) ? -convertedValue : convertedValue);
 }
 /**
-*is_letter - confirms is c is an alphabetic letter
-*@i: The character to input
-*Return: 1 if c is alphabetic, 0 otherwise
-*/
+ *is_letter - confirms is c is an alphabetic letter
+ *@i: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
 
 int is_letter(int i)
 {
-    return ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'));
+	return ((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z'));
 }
 
 /**
-* _delim - confirms if d is a delimeter character
-* @str: string of delimeter
-* @d: input char
-* Return: bool value true or false
-*/
+ * is_delim - confirms if d is a delimeter character
+ * @str: string of delimeter
+ * @d: input char
+ * Return: bool value true or false
+ */
 
 int is_delim(char d, char *str)
 {
-    while (*str != '\0') 
-    {
-        if (*str == d) 
-        {
-            return (1); /*true*/
-        }
-        str++;
-    }
-    return (0); /*false*/
+	while (*str != '\0')
+	{
+		if (*str == d)
+		{
+			return (1); /*true*/
+		}
+		str++;
+	}
+	return (0); /*false*/
 }
+
