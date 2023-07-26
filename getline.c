4844f50372d2c496f2d7d791ee4dc6e71031eb48
root <Annabel>
2023-07-26 05:48:03 -0700

    getline.c

--- getline.c
+++ getline.c
@@ -144,3 +144,14 @@ int _getline(info_t *info, char **ptr, size_t *length)
 	*ptr = p;
 	return (s);
 }
+/**
+ * sigintHandler - blocks ctrl-C
+ * @sig_num: the signal number
+ * Return: void
+ */
+void sigintHandler(__attribute__((unused))int sig_num)
+{
+	_puts("\n");
+	_puts("$ ");
+	_putchar(BUF_FLUSH);
+}
